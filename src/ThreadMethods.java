1a5a862983c517be621e307a1308171089422aac
neeta purohit <neetapurohit56@gmail.com>
2026-04-28 07:05:48 +0530

    Methods of thread in java

---
+++ src/ThreadMethods.java
@@ -0,0 +1,33 @@
+public class ThreadMethods extends Thread {
+//    java methods:- run(), start(), sleep(), join(),
+//    public void run(){
+//        System.out.println("Thread is running....");
+//        for (int i=1; i<=5; i++){
+//            System.out.println(i);
+//            try {
+//                sleep(2000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        System.out.println("Thread executed");
+//    }
+    public void run(){
+        for (int i=1; i<=5; i++){
+            System.out.println(Thread.currentThread().getName() + "-Priority: "+Thread.currentThread().getPriority() +
+                    "count: " + i);
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+    static void main(String[] args) throws InterruptedException {
+          ThreadMethods t1 = new ThreadMethods();
+          t1.start();
+          t1.join();//using this we'll stop for t1 to finish then we'll print hello otherwise it'll print hello first
+        System.out.println("hello");
+
+    }
+}
